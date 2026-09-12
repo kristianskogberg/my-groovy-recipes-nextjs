780dc900cb75721a33833b4dcb4ec735288e608e
Kristian Skogberg <k.skogberg98@gmail.com>
2026-09-12 13:27:29 +0300

    create nextjs app using supabase template

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
   : "http://localhost:3000";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "My Groovy Recipes",
+  description: "Save and discover your favorite recipes",
 };
 
 const geistSans = Geist({
